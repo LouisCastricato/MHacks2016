22cf1f636112e225e2943b107d338f1643b50882
Agastya Kalra <agastya.kalra@gmail.com>
2016-02-20 04:52:14 -0500

    lol

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ def rots(e1, e2, ordis=1):
 	xdif = e2[0]-e1[0]
 	yrot = math.atan(ydif/xdif)*(180/3.14159)
 	dis = (xdif**2 + ydif**2)**.5
-	if dis < 0.75*ordis:
+        print dis
+        print ordis
+	if dis < 0.8*ordis:
 		return[yrot,1]
 
 	return [yrot, 0]
@@ -91,6 +93,13 @@ if __name__ == '__main__':
     x_offset = 0
     y_offset = 0
     helper = cvHelper()
+    print 'yo'
+    a = True;
+    counter = 0; 
+    rcounter = 0;
+    yrot = 0;
+    zrot = 0;
+    ordis = 1;
     while True:
         # grab the current frame
         (grabbed, frame) = camera.read()
@@ -111,21 +120,41 @@ if __name__ == '__main__':
                     cv2.rectangle(frame, (eye[0],eye[1]),
                             (eye[0]+eye[2],eye[1]+eye[3]),
                             (155,55,200),2)
-	if len(detectedEyes) >= 2:
-	    vec1 = (2* detectedEyes[0][0] + 0.5 * detectedEyes[0][2],2* detectedEyes[0][1] + 0.5 * detectedEyes[0][3])
-	    vec2 = (2* detectedEyes[1][0] + 0.5 * detectedEyes[1][2],2* detectedEyes[1][1] + 0.5 * detectedEyes[1][3])
-            print rots(vec1,vec2)
-
-         
+        if len(detectedEyes) == 0:
+            print 'blink!'
+    	if len(detectedEyes) >= 2:
+    	    vec1 = (2* detectedEyes[0][0] + 0.5 * detectedEyes[0][2],2* detectedEyes[0][1] + 0.5 * detectedEyes[0][3])
+    	    vec2 = (2* detectedEyes[1][0] + 0.5 * detectedEyes[1][2],2* detectedEyes[1][1] + 0.5 * detectedEyes[1][3])
+            if a and counter > 10:
+                 print "to"
+                 ordis = ((vec1[0]-vec2[0])**2 + (vec1[1]-vec2[1])**2)**0.5
+                 a = False;
+            elif a:
+                counter += 1;
+            rotations = rots(vec1,vec2, ordis)
+            yrot += rotations[0];
+            zrot += rotations[1];
+            print zrot;
+            rcounter += 1;
+            if rcounter > 2:
+                 yrot = yrot/3;
+                 print yrot;
+                 if zrot > 0:
+                    print '3/4 view';
+                 else:
+                    print 'front view';
+                 yrot = 0;
+                 zrot = 0;
+                 rcounter = 0
         # show the frame and record if the user presses a key
-        #frame[y_offset:y_offset+img.shape[0], x_offset:x_offset+img.shape[1]] = img
+            #frame[y_offset:y_offset+img.shape[0], x_offset:x_offset+img.shape[1]] = img
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
 
-        # if the 'q' key is pressed, stop the loop
+            # if the 'q' key is pressed, stop the loop
         if key == ord("q"):
 		break
 
-    # cleanup the camera and close any open windows
+        # cleanup the camera and close any open windows
     camera.release()
     cv2.destroyAllWindows()
